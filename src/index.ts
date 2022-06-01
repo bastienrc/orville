397702ea506e39d713700a6f578ca8d7e2955845
Sebastien Colbert <colbertsebastien@gmail.com>
2022-06-01 22:15:11 +0200

    🐛 FIX: devTools fermé et menu enlevé

--- src/index.ts
+++ src/index.ts
@@ -20,8 +20,11 @@ const createWindow = (): void => {
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
+  // Remove the menu bar.
+  mainWindow.removeMenu()
+
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
